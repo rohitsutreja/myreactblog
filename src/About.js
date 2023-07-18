4c9aada229d4b2e796f8ef04ff525898cf32ed47
rohit <rohitsutreja@gmail.com>
2023-07-19 00:25:20 +0530

    complete

---
+++ src/About.js
@@ -0,0 +1,10 @@
+const About = () => {
+    return (
+        <main className='About'>
+            <h2>About</h2>
+            <p>This Blog app is created by Rohit in React using React Routers and Context API, I used Dave Gray's React Tutorials for reference.</p>
+        </main>
+    )
+}
+
+export default About
